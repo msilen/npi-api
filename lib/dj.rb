3e0b4b009af1b34cab49676699211196a8fb725b
Martin Silenus <sileenus@gmail.com>
2013-10-27 18:04:07 +0900

    continIous

--- lib/dj.rb
+++ lib/dj.rb
@@ -8,5 +8,7 @@ require 'active_record'
 require_relative 'env'
 
 #Nppes.background_init
-Nppes::Jobs::IniterJob.new.perform
-
+#Nppes::Jobs::IniterJob.new.perform
+binding.pry
+continious=false
+Nppes::Jobs::SearcherJob.new((Nppes.get_time_period if continious))
